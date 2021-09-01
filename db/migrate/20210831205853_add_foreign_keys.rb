e14513f6772564dd33357d13d4b5a55b8347a638
motst <abkhvan@gmail.com>
2021-09-01 10:35:14 +0300

    add models, set default values, set null constraints

---
+++ db/migrate/20210831205853_add_foreign_keys.rb
@@ -0,0 +1,6 @@
+class AddForeignKeys < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :tests, :category, foreign_key: true
+    add_reference :questions, :test, foreign_key: true
+  end
+end
