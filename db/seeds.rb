ce06fbc6841033bca2160a1186ee761e63ba9799
Aleksander <abkhvan@gmail.com>
2021-09-10 19:22:09 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create! ([
+  { name: 'Aleksander' },
+  { name: 'Stanislava' }
+])
+categories = Category.create! ([
+  { title: 'Cats' },
+  { title: 'Dogs' },
+  { title: 'Cartoons' }
+])
+tests = Test.create! ([
+  { title: 'Cats life', level: 3, category_id: categories[0].id },
+  { title: 'Other', level: 1, category_id: categories[0].id },
+  { title: 'Dogs in history', level: 3, category_id: categories[1].id },
+  { title: 'Russian cartoons', level: 1, category_id: categories[2].id },
+  { title: 'American cartoons', level: 2, category_id: categories[2].id }
+])
+questions = Question.create! ([
+  { body: 'How many percent of their lives do cats spend in a dream?', test_id: tests[0].id },
+  { body: 'What sound does a cat make when it is happy?', test_id: tests[0].id },
+  { body: 'What was the name of the first dogs who visited space?', test_id: tests[1].id },
+  { body: 'What did Carlson like to eat most of all?', test_id: tests[2].id },
+  { body: 'What was the name of the bulldog in the cartoon "Tom and Jerry"', test_id: tests[2].id }
+])
+answers = Answer.create! ([
+  { body: '75 percent', correct: true, question_id: questions[0].id },
+  { body: '45 percent', correct: false, question_id: questions[0].id },
+  { body: 'Pur, pur', correct: true, question_id: questions[1].id },
+  { body: 'Mau, mau', correct: false, question_id: questions[1].id },
+  { body: 'Belka and Strelka', correct: true, question_id: questions[2].id },
+  { body: 'Zhuchka and Grelka', correct: false, question_id: questions[2].id },
+  { body: 'Teftels', correct: true, question_id: questions[3].id },
+  { body: 'Honey', correct: false, question_id: questions[3].id },
+  { body: 'Spike', correct: true, question_id: questions[4].id },
+  { body: 'Rust', correct: false, question_id: questions[4].id }
+])
