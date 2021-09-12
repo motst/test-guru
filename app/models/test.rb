0678a7a6cd98d866398fb0144b1a93b3001d8409
Aleksander <abkhvan@gmail.com>
2021-09-13 00:29:50 +0300

    add sort by category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+
+  def self.tests_sort_by(category)
+    joins('JOIN categories ON categories.id = tests.category_id').
+    where(categories: {title: category}).
+    pluck(:title).
+    sort.reverse
+  end
 end
