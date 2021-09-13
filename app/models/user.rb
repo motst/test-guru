5242f48799ae6a2164a863511ba74bae4e125f0b
Aleksander <abkhvan@gmail.com>
2021-09-14 00:50:01 +0300

    change method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,8 @@
 class User < ApplicationRecord
 
-  def self.id
-    where(name: self.name).pluck(:id)
-  end
-
-  def add_user_id_to_tests(test_id)
-    current_users_id = Test.where(id: test_id).pluck(:users_id).join(',')
-    if current_users_id == ""
-      Test.find_by(id: test_id).update(users_id: self.id)
-    elsif !current_users_id.split(',').include?(self.id.to_s)
-      new_users_id = current_users_id << ',' + self.id.to_s
-      Test.find_by(id: test_id).update(users_id: new_users_id)
-    end
-  end
-
   def find_user_tests_by(level)
-    selected_tests = Test.where.not(users_id: nil).where(level: level).pluck(:title, :users_id)
-    if selected_tests != []
-      user_tests = []
-      selected_tests.each do |test|
-        user_tests << test[0] if test[1].split(',').include?(self.id.to_s)
-      end
-      user_tests
-    end
+    Test.joins('JOIN user_tests ON tests.id = user_tests.test_id').
+    where('user_tests.user_id = ? AND tests.level = ?', id, level)
   end
 end
+
