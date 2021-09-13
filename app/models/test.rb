d4b1cf3ed8d73403340646226f53b6c5e8ed8875
Aleksander <abkhvan@gmail.com>
2021-09-14 00:48:01 +0300

    add sql order

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   def self.tests_sort_by(category)
     joins('JOIN categories ON categories.id = tests.category_id').
     where(categories: {title: category}).
-    pluck(:title).
-    sort.reverse
+    order('tests.title DESC').
+    pluck(:title)
   end
 end
